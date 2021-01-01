54e16b5f5a157e56c2dc512b926cc8bc2910fce0
Mario Sanchez Carrion <mariosc@gmail.com>
2021-01-01 10:58:55 -0500

    add probate page

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync -av -e ssh habitatinvestors.com/* mariobox@104.131.108.216:/var/www/habitatinvestors.com/html
+rsync -av -e ssh ../habitatinvestors.com/* mariobox@104.131.108.216:/var/www/habitatinvestors.com/html
